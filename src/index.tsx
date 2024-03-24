55532bce244bb3a474573ebb2fdb87f9d7c6b1d4
Pawel Nackowski <pawelnackowski@gmail.com>
2024-03-24 15:55:26 +0100

    deleted strictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM, { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { GlobalStyle } from "./GlobalStyle";
 import { Provider } from "react-redux";
 import { ThemeProvider } from 'styled-components';
@@ -12,14 +12,12 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <App />
       </ThemeProvider>
     </Provider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
