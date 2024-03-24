aeeca2e915ea952b464586d6a226c6bdb03dd2c5
Pawel Nackowski <pawelnackowski@gmail.com>
2024-03-24 16:00:05 +0100

    added sagaMiddleware

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,18 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import tasksReducer from "./features/TasksPages/Tasks/tasksSlice";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./features/TasksPages/Tasks/rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
     tasks: tasksReducer,
   },
+  middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
